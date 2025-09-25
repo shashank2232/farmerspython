f0143f334b37eb90b3638483b06da17e559d9ea7
Shashank Mishra <95490636+shashank2232@users.noreply.github.com>
2025-09-25 10:23:56 +0530

    Update config.py

--- farmerapp/project/config.py
+++ farmerapp/project/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI ='postgresql://venkatesh@db:5432/farmer'
+    SQLALCHEMY_DATABASE_URI ='postgresql://shashank@db:5432/farmer'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
